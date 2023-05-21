9c92db4b1fdfb6d7a291b52b2a99c5f3f45f8bc0
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-05-21 15:02:19 +0600

    Getting data from mongodb to server

--- index.js
+++ index.js
@@ -25,6 +25,15 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toysCollection = client.db("kiddToY").collection("toys");
+
+    app.get("/alltoys", async (req, res) => {
+      const cursor = toysCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +41,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
